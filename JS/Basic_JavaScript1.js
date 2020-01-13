32505270ac1b66b43984615987bf37ce2d0df9f9
Gavin Kimberlin <guviner@gmail.com>
2020-01-13 14:29:45 -0800

    create a basic site using javascript.

---
+++ JS/Basic_JavaScript1.js
@@ -0,0 +1,6 @@
+var suh = "Hank said, \"Suh dood!\"", woah = "Dylan said, \"Woah dood!\"", woah = woah.fontcolor("green");
+document.write(suh + "<br>" + woah + "<br>")
+
+var x = 45
+var y = 55
+document.write(x + y)
