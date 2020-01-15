5b84773fa189a48f63b9447a574fd19c94398e8e
Gavin Kimberlin <guviner@gmail.com>
2020-01-14 23:33:11 -0800

    create a page using if, else if, and else statements.

--- JS/Basic_JavaScript_7.js
+++ JS/Basic_JavaScript_7.js
@@ -10,3 +10,26 @@ var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction() {
     document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color +"-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
+
+function get_Date() {
+    if (new Date().getHours() < 12 && Date().getHours() >= 6) {
+        document.getElementById("Greeting").innerHTML = "Good Morning!"
+    } else if (new Date().getHours() < 17) {
+        document.getElementById("Greeting").innerHTML = "Good Afternoon!"
+    } else if (new Date().getHours() < 20) {
+        document.getElementById("Greeting").innerHTML = "Good Evening!"
+    }else {
+        document.getElementById("Greeting").innerHTML = "Good Night!"
+    };
+}
+
+function num() {
+    number = document.getElementById("num_Input").value;
+    if (number > 50) {
+        document.getElementById("output").innerHTML = "Your number is greater than 50";
+    } else if (number < 50) {
+        document.getElementById("output").innerHTML = "Your number is less than 50";
+    } else {
+        document.getElementById("output").innerHTML = "Your number is 50";
+    };
+}
