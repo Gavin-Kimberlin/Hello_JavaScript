174a1a4c1b9c5e34383593d0993a931d22035c6c
Gavin Kimberlin <guviner@gmail.com>
2020-01-15 23:03:07 -0800

    add toFixed and valueOf methods.

--- JS/Basic_JavaScript_8.js
+++ JS/Basic_JavaScript_8.js
@@ -4,7 +4,10 @@ function someFunction() {
     var c = "a ";
     var d = "sentence.";
     var sentence = a.concat(b, c, d);
-    var section = sentence.slice(10,18)
+    var section = sentence.slice(10,18).valueOf();
+    var x = 45676767.4564564564526456;
+    var y = x.toFixed(4);
+    var z = 45555.454533333.toPrecision(3)
     document.getElementById("something").innerHTML = sentence;
-    document.getElementById("section").innerHTML = section;
+    document.getElementById("section").innerHTML = section + "<br><br>" + y.toString() + "<br>" + z;
 }
